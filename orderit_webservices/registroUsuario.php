06f72bae082ded4e93eeee475fc5dfd6c9174071
Ricardo Giron Colorado <ricardo.giron.colorado@gmail.com>
2021-12-05 21:20:10 -0600

    Nuevo respaldo de los WS

--- orderit_webservices/registroUsuario.php
+++ orderit_webservices/registroUsuario.php
@@ -1,13 +1,13 @@
 <?PHP
 include 'conexion.php';
-$idUsuario=$_POST['idUsuario'];
-$nombreUsuario=$_POST['nombreUsuario'];
-$apPaterno=$_POST['apPaterno'];
-$apMaterno=$_POST['apMaterno'];
-$email=$_POST['email'];
-$password=$_POST['password'];
 
-$consulta="INSERT INTO usuarios VALUES('".null."','".$nombreUsuario."','".$apPaterno."','".$apMaterno."','".$email."','".$password."')";
+$nombreUsuario=$_GET['nombreUsuario'];
+$apPaterno=$_GET['apPaterno'];
+$apMaterno=$_GET['apMaterno'];
+$email=$_GET['email'];
+$password=$_GET['password'];
+
+$consulta="INSERT INTO usuarios VALUES(null,'".$nombreUsuario."','".$apPaterno."','".$apMaterno."','".$email."','".$password."')";
 
 mysqli_query($conexion,$consulta) or die (mysqli_error());
 mysqli_close($conexion);
