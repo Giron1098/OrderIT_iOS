44d7206d0ff40c6acc67c25c5d6db1c286541a94
Ricardo Giron Colorado <ricardo.giron.colorado@gmail.com>
2021-12-06 17:12:04 -0600

    Errores solucionados, ajuste en los WS

--- OrderIT/Registro_ViewController.swift
+++ OrderIT/Registro_ViewController.swift
@@ -20,13 +20,8 @@ class Registro_ViewController: UIViewController {
     
     var const = Constantes()
     
-    struct User: Codable{
-        var idUsuario: Int?
-        var nombreUsuario:String?
-        var apPaterno:String?
-        var apMaterno:String?
-        var email:String?
-        var password:String?
+    struct Mensaje: Codable{
+        var mensaje:String?
     }
 
     override func viewDidLoad() {
@@ -42,9 +37,31 @@ class Registro_ViewController: UIViewController {
             if nombre_registro != "" && ap_paterno_registro != "" && ap_materno_registro != "" && correo_registro != "" && password_registro != ""
             {
                 let URL_New_User = "http://\(const.dir_ip)/orderit/registroUsuario.php?nombreUsuario=\(nombre_registro)&apPaterno=\(ap_paterno_registro)&apMaterno=\(ap_materno_registro)&email=\(correo_registro)&password=\(password_registro)"
-                
-                registrarUsuario(URL: URL_New_User)
                 //print(nombre_registro,ap_paterno_registro,ap_paterno_registro,correo_registro,password_registro)
+                
+                print(URL_New_User)
+                    
+                AF.request(URL_New_User, method: .get, encoding: URLEncoding.default, headers: nil, interceptor: nil).response { ( responseData ) in
+                        guard let data = responseData.data else { return }
+                        do
+                        {
+                            let mensaje = try JSONDecoder().decode(Mensaje.self, from: data)
+                            
+                            if let mensaje = mensaje.mensaje
+                            {
+                                if mensaje == "Correo ya registrado"
+                                {
+                                    self.showEmailErrorAlert(mensaje: mensaje)
+                                } else if mensaje == "Usuario registrado exitosamente"
+                                {
+                                    self.showCompletitionRAlert(mensaje: mensaje)
+                                }
+                            }
+                        } catch {
+                            print("Error decoding: \(error)")
+                        }
+                }
+                
             } else {
                 showNoEmptyFieldsAlert()
             }
@@ -52,22 +69,10 @@ class Registro_ViewController: UIViewController {
         
     }
     
-    func registrarUsuario(URL:String)
-    {
-        print(URL)
-            
-        AF.request(URL, method: .get, encoding: URLEncoding.default, headers: nil, interceptor: nil).response { ( responseData ) in
-                guard let data = responseData.data else { return }
-                print(data)
-        }
-        
-        self.showCompletitionRAlert()
-    }
-        
     //Funcion para mostrar un alert en caso de que el usuario se registre correctamente
-    func showCompletitionRAlert()
+    func showCompletitionRAlert(mensaje:String)
     {
-        let alerta = UIAlertController(title: "Completado", message:"Usuario registrado correctamente", preferredStyle: .alert)
+        let alerta = UIAlertController(title: "Completado", message:mensaje, preferredStyle: .alert)
         
         let actionAceptar = UIAlertAction(title: "Aceptar", style:.default) { (_) in
             
@@ -90,6 +95,18 @@ class Registro_ViewController: UIViewController {
     func showNoEmptyFieldsAlert ()
     {
         let alerta = UIAlertController(title: "Hubo un problema", message:"No se permiten campos vacíos", preferredStyle: .alert)
+        
+        let actionAceptar = UIAlertAction(title: "Aceptar", style:.default, handler: nil)
+        
+        alerta.addAction(actionAceptar)
+        
+        present(alerta, animated: true, completion: nil)
+    }
+    
+    //Funcion para mostrar un alert en caso de que el correo ya esté registrado
+    func showEmailErrorAlert(mensaje:String)
+    {
+        let alerta = UIAlertController(title: "Hubo un problema", message:mensaje, preferredStyle: .alert)
         
         let actionAceptar = UIAlertAction(title: "Aceptar", style:.default, handler: nil)
         
--- orderit_webservices/registroUsuario.php
+++ orderit_webservices/registroUsuario.php
@@ -1,14 +1,35 @@
 <?PHP
+error_reporting(E_ERROR | E_PARSE);
 include 'conexion.php';
-
+$idUsuario=$_GET['idUsuario'];
 $nombreUsuario=$_GET['nombreUsuario'];
 $apPaterno=$_GET['apPaterno'];
 $apMaterno=$_GET['apMaterno'];
 $email=$_GET['email'];
 $password=$_GET['password'];
 
-$consulta="INSERT INTO usuarios VALUES(null,'".$nombreUsuario."','".$apPaterno."','".$apMaterno."','".$email."','".$password."')";
+if($email != "") {
+    $sentencia=$conexion->prepare("SELECT * FROM usuarios WHERE email=?");
+	$sentencia->bind_param('s',$email);
+	$sentencia->execute();
+	$resultado = $sentencia->get_result();
+    $num_rows = mysqli_num_rows($resultado);
+    if($num_rows >= 1){
+		$msg = new stdClass();
+		$msg->mensaje = "Correo ya registrado";
+		$msgJSON = json_encode($msg,JSON_UNESCAPED_UNICODE);
 
-mysqli_query($conexion,$consulta) or die (mysqli_error());
-mysqli_close($conexion);
+		echo $msgJSON;
+    }else{
+		$msg = new stdClass();
+		$msg->mensaje = "Usuario registrado exitosamente";
+		$msgJSON = json_encode($msg,JSON_UNESCAPED_UNICODE);
+
+		echo $msgJSON;
+
+        $consulta="INSERT INTO usuarios VALUES(null,'".$nombreUsuario."','".$apPaterno."','".$apMaterno."','".$email."','".$password."')";
+        mysqli_query($conexion,$consulta) or die (mysqli_error($conexion));
+		mysqli_close($conexion);
+    }
+}
 ?>
